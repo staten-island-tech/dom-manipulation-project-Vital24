06122856ea55ebd8daf5b140ec7598eb8db33569
Vital24 <zynn00001@gmail.com>
2022-11-02 10:16:35 -0400

    Notes1

---
+++ index.js
@@ -0,0 +1,18 @@
+const button = document.getElementById("btn");
+const text = document.querySelector("#text");
+
+console.log(text);
+
+const Domsel = {
+  button: document.getElementById("btn"),
+  text: document.querySelector("#text"),
+  empty: document.querySelectorAll(".point"),
+};
+
+console.log(Domsel);
+
+function backandtext(background, text) {
+    background.style.background = "red";
+
+    text.style.
+}
