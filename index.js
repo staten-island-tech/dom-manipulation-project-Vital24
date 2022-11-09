750971cff67c90a3aa63214b69eb274ed42c0b03
Vital24 <zynn00001@gmail.com>
2022-11-09 10:16:38 -0500

    Learn

--- index.js
+++ index.js
@@ -1,27 +1,7 @@
 const Domsel = {
   button: document.getElementById("btn"),
-  text: document.querySelector("#text"),
-  box: document.querySelector("#bbbox"),
-  points: document.querySelectorAll(".point"), //node list
+  box: document.getElementById("container-box"),
+  points: document.querySelectorAll("#input"),
 };
 
-function backandtext(background, text) {
-  background.style.backgroundColor = "purple";
-  text.innerHTML = "this is now a big red box";
-  text.style.fontSize = "40px";
-  text.style.color = "white";
-}
-
-Domsel.button.addEventListener("click", function () {
-  backandtext(Domsel.box, Domsel.text);
-}); // takes two agruements, event and callback function
-
-function changeLi() {
-  Domsel.points.forEach((point) => {
-    point.addEventListener(click, function () {
-      point.textContent = "NA Pokemon";
-    });
-    console.log(point);
-  });
-}
-changeLi();
+Domsel.box.insertAdjacentElement("beforebegin", `<p>We are H1</p>`);
