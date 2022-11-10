545ef4ca82c7c311ce28a9a1b1440da9c57dfcbe
Vital24 <zynn00001@gmail.com>
2022-11-10 10:14:14 -0500

    Confused

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-const Domsel = {
+const DOMselectors = {
   button: document.getElementById("btn"),
-  box: document.getElementById("container-box"),
-  points: document.querySelectorAll("#input"),
+  box: document.querySelector(".container-box"),
+  input: document.querySelectorAll("#input"),
 };
 
-Domsel.box.insertAdjacentElement("beforebegin", `<p>We are H1</p>`);
+const Info = {
+  fname: "",
+  lname: "",
+  age: "",
+  gender: "",
+  race: "",
+};
+
+DOMselectors.button.addEventListener("click", function () {
+  let input = DOMselectors.input.value;
+  DOMselectors.box.insertAdjacentHTML("beforeend", `<p>${input}</p>`);
+  DOMselectors.input.value = "";
+
+  console.log(input);
+});
