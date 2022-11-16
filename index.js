844d74f48350e914ffcdf68aae68a3c12b6a04a4
Vital24 <zynn00001@gmail.com>
2022-11-16 10:13:55 -0500

    Confusion

--- index.js
+++ index.js
@@ -4,14 +4,6 @@ const DOMselectors = {
   input: document.querySelectorAll(".text-box"),
 };
 
-// const Info = {
-//   fname: "",
-//   lname: "",
-//   age: "",
-//   gender: "",
-//   grade: "",
-// };
-
 console.log(
   DOMselectors.submit,
   DOMselectors.button,
@@ -23,14 +15,21 @@ DOMselectors.submit.addEventListener("submit", function (abc) {
 
   let input = DOMselectors.input; //array of results/values
   let person = {};
+  let img = {};
   console.log(input[0]);
   person.firstName = input[0].value;
   person.lastName = input[1].value;
   person.gender = input[2].value;
+  person.music = input[3].value;
+  person.color = input[4].value;
+  person.picture = input[5].value;
+  img.src = `"${person.picture}"`;
   console.log(person);
 
   DOMselectors.box.insertAdjacentHTML(
     "beforeend",
-    `<H6>First name: ${person.firstName} \nLast name: ${person.lastName}</H6>`
+    `<H5>First name: ${person.firstName}<br>Last name: ${person.lastName}<br>Gender: ${person.gender}<br>Favorite Music: ${person.music}<br>Favorite Color: ${person.color}</H5>`
   );
+  console.log(input[5].value);
+  DOMselectors.box.append(img.src);
 });
