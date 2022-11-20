e9b96cb246857f16feadf26296190d58c4c224b7
Vital24 <113121145+Vital24@users.noreply.github.com>
2022-11-20 09:43:20 -0500

    Im so lost

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const DOMselectors = {
   submit: document.querySelector("#form"),
   box: document.querySelector(".info-card"),
   input: document.querySelectorAll(".text-box"),
+  remove: document.querySelector(".remove"),
 };
 
 console.log(
@@ -10,6 +11,28 @@ console.log(
   DOMselectors.box,
   DOMselectors.input
 );
+
+function clear() {
+  document.getElementById("form").reset();
+}
+
+function remove() {
+  document.get;
+}
+
+function pop() {
+  // select the target element
+  const last = document.getElementById("dos");
+  // remove the last list item
+  last.parentElement.removeChild(last);
+  const dog = document.getElementById("uno");
+  // remove the last list item
+  dog.parentElement.removeChild(dog);
+  const cat = document.getElementById("tres");
+  // remove the last list item
+  cat.parentElement.removeChild(cat);
+}
+
 DOMselectors.submit.addEventListener("submit", function (abc) {
   abc.preventDefault();
 
@@ -22,11 +45,25 @@ DOMselectors.submit.addEventListener("submit", function (abc) {
   person.music = input[3].value;
   person.color = input[4].value;
   person.picture = input[5].value;
+  let ids = 1;
+  ids++;
+  console.log(ids);
+  clear();
 
   DOMselectors.box.insertAdjacentHTML(
     "beforeend",
 
-    `<img class="info-img" src="${person.picture}"/>
-            <H5>First name: ${person.firstName}<br>Last name: ${person.lastName}<br>Gender: ${person.gender}<br>Favorite Music: ${person.music}<br>Favorite Color: ${person.color}</H5>`
+    `
+
+    <img id="tres" class="info-img" src="${person.picture}"/>
+    
+    <H5 id="dos">First name: ${person.firstName}<br>Last name: ${person.lastName}<br>Gender: ${person.gender}<br>Favorite Music: ${person.music}<br>Favorite Color: ${person.color}</H5>
+    
+    <button id="uno" class="remove">Remove</>`
   );
+  console.log(person);
+});
+
+DOMselectors.remove.addEventListener("click", function () {
+  pop();
 });
