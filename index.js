904055bdb2b23f1ce990587806da8bac2a0f30bc
Vital24 <113121145+Vital24@users.noreply.github.com>
2022-11-20 11:16:22 -0500

    Confusion

--- index.js
+++ index.js
@@ -16,23 +16,18 @@ function clear() {
   document.getElementById("form").reset();
 }
 
-function remove() {
-  document.get;
-}
-
 function pop() {
+  let input = DOMselectors.input; //array of results/values
+  let person = {};
+  console.log(input[0]);
+  person.firstName = input[0].value;
+  let idss = `#${person.firstName}`;
   // select the target element
-  const last = document.getElementById("dos");
+  let removal = document.getElementById(idss);
   // remove the last list item
-  last.parentElement.removeChild(last);
-  const dog = document.getElementById("uno");
-  // remove the last list item
-  dog.parentElement.removeChild(dog);
-  const cat = document.getElementById("tres");
-  // remove the last list item
-  cat.parentElement.removeChild(cat);
+  removal.removeChild(removal);
+  console.log("poop");
 }
-
 DOMselectors.submit.addEventListener("submit", function (abc) {
   abc.preventDefault();
 
@@ -45,21 +40,19 @@ DOMselectors.submit.addEventListener("submit", function (abc) {
   person.music = input[3].value;
   person.color = input[4].value;
   person.picture = input[5].value;
-  let ids = 1;
-  ids++;
-  console.log(ids);
   clear();
 
   DOMselectors.box.insertAdjacentHTML(
     "beforeend",
 
     `
-
-    <img id="tres" class="info-img" src="${person.picture}"/>
+    <div id="${person.firstName}>
+    <img class="info-img" src="${person.picture}"/>
     
-    <H5 id="dos">First name: ${person.firstName}<br>Last name: ${person.lastName}<br>Gender: ${person.gender}<br>Favorite Music: ${person.music}<br>Favorite Color: ${person.color}</H5>
+    <H5 >First name: ${person.firstName}<br>Last name: ${person.lastName}<br>Gender: ${person.gender}<br>Favorite Music: ${person.music}<br>Favorite Color: ${person.color}</H5>
     
-    <button id="uno" class="remove">Remove</>`
+    <button class="remove">Remove</>
+    </div>`
   );
   console.log(person);
 });
