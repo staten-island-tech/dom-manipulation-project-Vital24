3a89e10e6a27f329b571386fa196c31e78d8acb5
Vital24 <113121145+Vital24@users.noreply.github.com>
2023-02-22 09:28:07 -0500

    AA

--- index.js
+++ index.js
@@ -8,19 +8,6 @@ function clear() {
   document.getElementById("form").reset();
 }
 
-function info() {
-  let input = DOMselectors.input; //array of results/values
-  let person = {};
-  console.log(input[0]);
-  person.firstName = input[0].value;
-  person.lastName = input[1].value;
-  person.gender = input[2].value;
-  person.music = input[3].value;
-  person.color = input[4].value;
-  person.picture = input[5].value;
-  return person;
-}
-
 function removal(event) {
   event.target.parentElement.remove();
 }
